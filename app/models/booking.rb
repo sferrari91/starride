25940ac515d06232496630a8f1715bddfb5738e0
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-11-21 18:01:19 +0100

    modified start_date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :spaceship
-  validates start_date:, presence: true
-  validates end_date:, presence: true, comparison: { greater_than: :end_date }
+  validates :start_date, presence: true
+  validates :end_date, presence: true, comparison: { greater_than: :end_date }
 end
