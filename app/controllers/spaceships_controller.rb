4eae7d2dd7b245ed551f10f125ec3fb3aae3de9f
nicola magro <nicola.magro89@gmail.com>
2022-11-22 14:42:44 +0100

    spaceship-show-and-index

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,9 +1,11 @@
 class SpaceshipsController < ApplicationController
 
   def index
+    @spaceships = Spaceship.all
   end
 
   def show
+    @spaceship = Spaceship.find(params[:id])
   end
 
   def new
