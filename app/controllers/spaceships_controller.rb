8ddac543628d7fdb67d1efe7e4a1898fbd61bd05
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-11-22 12:43:23 +0100

    form working, spaceship controller added

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -4,6 +4,24 @@ class SpaceshipsController < ApplicationController
   end
 
   def show
+  end
 
+  def new
+    @spaceship = Spaceship.new
+  end
+
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    if @spaceship.save
+      redirect_to spaceship_path(@spaceship)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :photo, :description, :number_of_passenger, :price_per_day, :planet)
   end
 end
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -3,5 +3,5 @@ class Spaceship < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :planet, presence: true
-  validates :number_of_passengers, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :number_of_passenger, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
